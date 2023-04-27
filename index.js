cf50f014d87e3a76438893a0271f81537fb6d073
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-04-27 10:46:12 -0500

    added call function, correcting spelling issues, ran test and fix errors

--- index.js
+++ index.js
@@ -8,20 +8,20 @@ const { Triangle, Square, Circle } = require("./lib/shapes.js");
 //Function writes to an SVG file
 function writeToFile(fileName, answers) {
   let svgString = "";
-  svgString = `<svg version="1.1" baseProfile="full" width="300" height="300" xmlns="http://www.w3.org/2000/svg">`;
+  svgString = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">`;
   svgString += "<g>";
   svgString += `${answers.shape}`;
   //check user inputs for shape and color
   let shapeChoice;
   if (answers.shape === "circle") {
     shapeChoice = new Circle();
-    svg += `< circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundcolor}"/>`;
+    svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
   } else if (answers.shape === "square") {
     shapeChoice = new Square();
-    svg += `<rect x="73" y="40" width="154" height="154" fill="${answers.shapeBackgroundcolor}"/>`;
+    svgString += `<rect x="73" y="40" width="154" height="154" fill="${answers.shapeBackgroundColor}"/>`;
   } else {
     shapeChoice = new Triangle();
-    svgString += `<polygon points="150,40 73,250 227,250" fill="${answers.shapeBackgroundcolor}"/>`;
+    svgString += `<polygon points="150,40 73,250 227,250" fill="${answers.shapeBackgroundColor}"/>`;
   }
   //text alignment
   svgString += `<text x="150" y="150" font-size="35" fill="${answers.textColor}" text-anchor="middle">${answers.text}</text>`;
@@ -63,5 +63,19 @@ function promptUser() {
         "what color would you the background to be?(Color keyword or a hexadecimal number)",
       name: "shapeBackgroundColor",
     },
-  ]);
+  ])
+  //check user input for text length
+  .then ((answers) => {
+    if(answers.text.length > 3){
+      console.log("Please enter up to three characters");
+      promptUser();
+    }else{
+      writeToFile("logo.svg", answers);
+    }
+  });
 }
+
+// Function call to initialize app
+promptUser();
+
+
